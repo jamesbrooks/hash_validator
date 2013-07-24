25ba8010efbddc6686f5531afdc6874e1f84038f
James Brooks <james@gooddogdesign.com>
2013-07-24 12:02:30 +0930

    Removed 2.0.0 new lambda style syntax from tests to ensure 1.9.3 tests successfully

--- spec/validators/lambda_spec.rb
+++ spec/validators/lambda_spec.rb
@@ -6,10 +6,6 @@ describe 'Functional validator' do
       validate({}, { foo: lambda { |arg| } })
     end
 
-    it 'should accept a lambda (new syntax)' do
-      validate({}, { foo: -> (arg) {} })
-    end
-
     it 'should accept a proc' do
       validate({}, { foo: Proc.new { |arg| } })
     end
@@ -17,20 +13,20 @@ describe 'Functional validator' do
 
   describe 'Correct number of arguments for lambads in validations' do
     it 'should accept a lambda with one argument' do
-      expect { validate({}, { foo: -> (arg) {} }) }.to_not raise_error
+      expect { validate({}, { foo: lambda { |a| } }) }.to_not raise_error
     end
 
     it 'should not accept a lambda with no arguments' do
-      expect { validate({}, { foo: -> {} }) }.to raise_error(HashValidator::Validator::LambdaValidator::InvalidArgumentCount)
+      expect { validate({}, { foo: lambda { } }) }.to raise_error(HashValidator::Validator::LambdaValidator::InvalidArgumentCount)
     end
 
     it 'should not accept a lambda with two arguments' do
-      expect { validate({}, { foo: -> (a,b) {} }) }.to raise_error(HashValidator::Validator::LambdaValidator::InvalidArgumentCount)
+      expect { validate({}, { foo: lambda { |a,b| } }) }.to raise_error(HashValidator::Validator::LambdaValidator::InvalidArgumentCount)
     end
   end
 
   describe '#validate' do
-    let(:validations) {{ number: -> (n) { n.odd? } }}
+    let(:validations) {{ number: lambda { |n| n.odd? } }}
 
     it 'should validate true when the number is odd' do
       validate({ number: 1 }, validations).valid?.should be_true
@@ -42,7 +38,7 @@ describe 'Functional validator' do
   end
 
   describe 'Thrown exceptions from within the lambda' do
-    let(:validations) {{ number: -> (n) { n.odd? } }}
+    let(:validations) {{ number: lambda { |n| n.odd? } }}
 
     it 'should validate false when an exception occurs within the lambda' do
       validate({ number: '2' }, validations).valid?.should be_false
